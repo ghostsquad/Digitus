6588a3e242c48d34c446ab3c0f563be85da04000
Wes <ghost.squadron@gmail.com>
2013-11-13 19:33:21 -0800

    added Wrap string extension method (100% code coverage)

---
+++ digitus.test/StringExtensionsTests.cs
@@ -0,0 +1,110 @@
+﻿namespace digitus.test
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    ///     The string extensions tests.
+    /// </summary>
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class StringExtensionsTests
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        ///     The wrap_ given long wrap length_ expect minimal splitting.
+        /// </summary>
+        [TestMethod]
+        public void Wrap_GivenLongWrapLength_ExpectMinimalSplitting()
+        {
+            // arrange
+            string startString = "the quick brown fox jumped over the lazy dog.";
+            int maxLineLength = 10;
+            string expectedString = string.Format(
+                "the quick{0}brown fox{0}jumped{0}over the{0}lazy dog.", 
+                Environment.NewLine);
+
+            // act
+            string actualString = startString.Wrap(maxLineLength);
+
+            // assert
+            Assert.AreEqual(expectedString, actualString);
+        }
+
+        /// <summary>
+        ///     The wrap_ given possibility to split mid word_ expect split before.
+        /// </summary>
+        [TestMethod]
+        public void Wrap_GivenPossibilityToSplitMidWord_ExpectSplitBefore()
+        {
+            // arrange
+            string startString = "say hello to my little friend";
+            int maxLineLength = 4;
+            string expectedString = startString.Replace(" ", Environment.NewLine);
+
+            // act
+            string actualString = startString.Wrap(maxLineLength);
+
+            // assert
+            Assert.AreEqual(expectedString, actualString);
+        }
+
+        /// <summary>
+        ///     The wrap_ given string to wrap_ expect two lines.
+        /// </summary>
+        [TestMethod]
+        public void Wrap_GivenStringToWrap_ExpectTwoLines()
+        {
+            // arrange
+            string startString = "hello world";
+            int maxLineLength = 5;
+            string expectedString = startString.Replace(" ", Environment.NewLine);
+
+            // act
+            string actualString = startString.Wrap(maxLineLength);
+
+            // assert
+            Assert.AreEqual(expectedString, actualString);
+        }
+
+        /// <summary>
+        ///     The wrap_ given unevenly wrappable string_ expect split at minimum.
+        /// </summary>
+        [TestMethod]
+        public void Wrap_GivenUnevenlyWrappableString_ExpectSplitAtMinimum()
+        {
+            // arrange
+            string startString = "say hello to my little friend";
+            int maxLineLength = 6;
+            string expectedString = "say\r\nhello\r\nto my\r\nlittle\r\nfriend";
+
+            // act
+            string actualString = startString.Wrap(maxLineLength);
+
+            // assert
+            Assert.AreEqual(expectedString, actualString);
+        }
+
+        /// <summary>
+        ///     The wrap_ given unwrappable string_ expect one line.
+        /// </summary>
+        [TestMethod]
+        public void Wrap_GivenUnwrappableString_ExpectOneLine()
+        {
+            // arrange
+            string startString = "helloworld";
+            int maxLineLength = 5;
+
+            // act
+            string actualString = startString.Wrap(maxLineLength);
+
+            // assert
+            Assert.AreEqual(startString, actualString);
+        }
+
+        #endregion
+    }
+}
---
+++ digitus/StringExtensions.cs
@@ -0,0 +1,85 @@
+﻿namespace digitus
+{
+    using System;
+    using System.Text;
+
+    /// <summary>
+    /// The string extensions.
+    /// </summary>
+    public static class StringExtensions
+    {
+        #region Constants
+
+        /// <summary>
+        /// The default wrap on.
+        /// </summary>
+        private const char DefaultWrapOn = ' ';
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The wrap.
+        /// </summary>
+        /// <param name="theString">
+        /// The the string.
+        /// </param>
+        /// <param name="maxLineLength">
+        /// The max line length.
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        public static string Wrap(this string theString, int maxLineLength)
+        {
+            return theString.Wrap(DefaultWrapOn, maxLineLength);
+        }
+
+        /// <summary>
+        /// The wrap.
+        /// </summary>
+        /// <param name="theString">
+        /// The the string.
+        /// </param>
+        /// <param name="wrapOn">
+        /// The wrap on.
+        /// </param>
+        /// <param name="maxLineLength">
+        /// The max line length.
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        public static string Wrap(this string theString, char wrapOn, int maxLineLength)
+        {
+            var wrappedStringBuilder = new StringBuilder();
+
+            int startIndex = 0;
+            int lastIndexOfWrapOnCharacter = -1;
+
+            for (int i = 0; i < theString.Length; i++)
+            {
+                if (theString[i] == wrapOn)
+                {
+                    lastIndexOfWrapOnCharacter = i;
+                }
+
+                if (maxLineLength - (i - startIndex) <= 0 && lastIndexOfWrapOnCharacter > 0)
+                {
+                    wrappedStringBuilder.Append(
+                        theString.Substring(startIndex, lastIndexOfWrapOnCharacter - startIndex));
+                    wrappedStringBuilder.Append(Environment.NewLine);
+                    startIndex = lastIndexOfWrapOnCharacter + 1;
+                    lastIndexOfWrapOnCharacter = -1;
+                }
+            }
+
+            wrappedStringBuilder.Append(theString.Substring(startIndex, theString.Length - startIndex));
+
+            return wrappedStringBuilder.ToString();
+        }
+
+        #endregion
+    }
+}
